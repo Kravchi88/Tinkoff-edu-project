d64547303fc56b1be5b6aab7dfd6c9c3e948bda4
Ilya <ikrav2003@mail.ru>
2023-03-31 00:21:29 +0300

    moved annotation to ScrapperApplication

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/scheduler/LinkUpdaterScheduler.java
@@ -2,10 +2,8 @@ package ru.tinkoff.edu.java.scrapper.scheduler;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-@EnableScheduling
 public class LinkUpdaterScheduler {
     private static final Logger logger = LogManager.getLogger(LinkUpdaterScheduler.class);
 
